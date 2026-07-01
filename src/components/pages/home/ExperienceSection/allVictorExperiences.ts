838c370899e27fdc5412de9ed511ebfee7456713
victorcamargo <victoryuritc@yahoo.com>
2026-07-01 02:30:37 -0300

    refactor: refine text

--- src/components/pages/home/ExperienceSection/allVictorExperiences.ts
+++ src/components/pages/home/ExperienceSection/allVictorExperiences.ts
@@ -65,51 +65,51 @@ export const allVictorExperiences: (VictorExperienceType & {
       photos: [
         {
           photo: {
-            alt: "AAK Tele-Science - Select user type screen",
+            alt: "AAK Tele-Science - Select User Type Screen",
             src: "/companies-portfolio-photos/aak-telescience/1-aak-telescience-select-user-type.png",
           },
           type: "photo",
-          title: "Select user type screen",
+          title: "Select User Type Screen",
         },
         {
           photo: {
-            alt: "AAK Tele-Science - Create user screen",
+            alt: "AAK Tele-Science - Create User Screen",
             src: "/companies-portfolio-photos/aak-telescience/2-aak-telescience-create-user.png",
           },
           type: "photo",
-          title: "Create user screen",
+          title: "Create User Screen",
         },
         {
           photo: {
-            alt: "AAK Tele-Science - Profile settings screen",
+            alt: "AAK Tele-Science - Profile Settings Screen",
             src: "/companies-portfolio-photos/aak-telescience/3-aak-telescience-profile-settings.png",
           },
           type: "photo",
-          title: "Profile settings screen",
+          title: "Profile Settings Screen",
         },
         {
           photo: {
-            alt: "AAK Tele-Science - Researcher profile screen",
+            alt: "AAK Tele-Science - Researcher Profile Screen",
             src: "/companies-portfolio-photos/aak-telescience/4-aak-telescience-researcher-profile.png",
           },
           type: "photo",
-          title: "Researcher profile screen",
+          title: "Researcher Profile Screen",
         },
         {
           photo: {
-            alt: "AAK Tele-Science - Connection requests screen",
+            alt: "AAK Tele-Science - Connection Requests Screen",
             src: "/companies-portfolio-photos/aak-telescience/5-aak-telescience-connection-requests.png",
           },
           type: "photo",
-          title: "Connection requests screen",
+          title: "Connection Requests Screen",
         },
         {
           photo: {
-            alt: "AAK Tele-Science - Create blog screen",
+            alt: "AAK Tele-Science - Create Blog Screen",
             src: "/companies-portfolio-photos/aak-telescience/6-aak-telescience-create-blog.png",
           },
           type: "photo",
-          title: "Create blog screen",
+          title: "Create Blog Screen",
         },
       ],
     },
@@ -171,56 +171,56 @@ export const allVictorExperiences: (VictorExperienceType & {
       photos: [
         {
           photo: {
-            alt: "Gigawrks - Agents home screen preview",
+            alt: "Gigawrks - Agents Home Screen Preview",
             src: "/companies-portfolio-photos/gigawrks/gigawrks-agents-home-screen.png",
           },
           type: "photo",
-          title: "Agents home screen",
+          title: "Agents Home Screen",
         },
 
         {
           photo: {
-            alt: "Gigawrks - Clients home screen preview",
+            alt: "Gigawrks - Clients Home Screen Preview",
             src: "/companies-portfolio-photos/gigawrks/gigawrks-clients-home-screen-1.png",
           },
           type: "photo",
-          title: "Clients home screen",
+          title: "Clients Home Screen",
         },
 
         {
           photo: {
-            alt: "Gigawrks - Clients home screen items preview",
+            alt: "Gigawrks - Clients Home Screen Items Preview",
             src: "/companies-portfolio-photos/gigawrks/gigawrks-clients-home-screen-2.png",
           },
           type: "photo",
-          title: "Clients home screen items",
+          title: "Clients Home Screen Items",
         },
 
         {
           photo: {
-            alt: "Gigawrks - Track transaction screen preview",
+            alt: "Gigawrks - Track Transaction Screen Preview",
             src: "/companies-portfolio-photos/gigawrks/gigawrks-track-transaction-screen.png",
           },
           type: "photo",
-          title: "Track transaction screen",
+          title: "Track Transaction Screen",
         },
 
         {
           photo: {
-            alt: "Gigawrks - Chat screen preview",
+            alt: "Gigawrks - Chat Screen Preview",
             src: "/companies-portfolio-photos/gigawrks/gigawrks-chat-screen.png",
           },
           type: "photo",
-          title: "Chat screen",
+          title: "Chat Screen",
         },
 
         {
           photo: {
-            alt: "Gigawrks - Notifications list screen preview",
+            alt: "Gigawrks - Notifications List Screen Preview",
             src: "/companies-portfolio-photos/gigawrks/gigawrks-notifications-list-screen.png",
           },
           type: "photo",
-          title: "Notifications list screen",
+          title: "Notifications List Screen",
         },
       ],
     },
@@ -319,7 +319,7 @@ export const allVictorExperiences: (VictorExperienceType & {
             alt: "Aprova Concursos - Institution Screen",
             src: "/companies-portfolio-photos/iesde/1-aprova-concursos-institution-screen.png",
           },
-          title: "Home screen",
+          title: "Institution Screen",
           type: "photo",
         },
 
@@ -328,7 +328,7 @@ export const allVictorExperiences: (VictorExperienceType & {
             alt: "Aprova Concursos - Course Screen",
             src: "/companies-portfolio-photos/iesde/2-aprova-concursos-course-screen.png",
           },
-          title: "Course screen",
+          title: "Course Screen",
           type: "photo",
         },
 
@@ -337,16 +337,16 @@ export const allVictorExperiences: (VictorExperienceType & {
             alt: "Questões+ Webapp - Home Screen",
             src: "/companies-portfolio-photos/iesde/3-questoes-mais-webapp-home-screen.png",
           },
-          title: "Webapp Home Screen",
+          title: "Questões+ Webapp Home Screen",
           type: "photo",
         },
 
         {
           photo: {
-            alt: "Aprova Concursos - Search Screen",
+            alt: "Questões+ Webapp - Search Screen",
             src: "/companies-portfolio-photos/iesde/4-aprova-concursos-search-screen.png",
           },
-          title: "Search Screen",
+          title: "Questões+ Webapp Search Screen",
           type: "photo",
         },
 
@@ -355,7 +355,7 @@ export const allVictorExperiences: (VictorExperienceType & {
             alt: "LMS Aprova Concursos - Login Screen",
             src: "/companies-portfolio-photos/iesde/5-lms-aprova-concursos-login-screen.png",
           },
-          title: "Login Screen",
+          title: "LMS Aprova Concursos - Login Screen",
           type: "photo",
         },
       ],
@@ -441,151 +441,151 @@ export const allVictorExperiences: (VictorExperienceType & {
             src: "/companies-portfolio-photos/groner/1-groner-departments.png",
           },
           type: "photo",
-          title: "Departments screen",
+          title: "Departments Screen",
         },
         {
           photo: {
-            alt: "Groner - Custom variables screen",
+            alt: "Groner - Custom Variables Screen",
             src: "/companies-portfolio-photos/groner/2-groner-custom-variables.png",
           },
           type: "photo",
-          title: "Custom variables screen",
+          title: "Custom Variables Screen",
         },
         {
           photo: {
-            alt: "Groner - Survey list screen",
+            alt: "Groner - Survey List Screen",
             src: "/companies-portfolio-photos/groner/3-groner-survey-list.png",
           },
           type: "photo",
-          title: "Survey list screen",
+          title: "Survey List Screen",
         },
         {
           photo: {
-            alt: "Groner - Sent emails screen",
+            alt: "Groner - Sent Emails Screen",
             src: "/companies-portfolio-photos/groner/4-groner-sent-emails.png",
           },
           type: "photo",
-          title: "Sent emails screen",
+          title: "Sent Emails Screen",
         },
         {
           photo: {
-            alt: "Groner - Schedule task linked with Google Agenda",
+            alt: "Groner - Schedule Task Linked with Google Agenda",
             src: "/companies-portfolio-photos/groner/5-groner-schedule-task-linked-with-google-agenda.png",
           },
           type: "photo",
-          title: "Schedule task linked with Google Agenda",
+          title: "Schedule Task Linked with Google Agenda",
         },
         {
           photo: {
-            alt: "Groner - Solar panels installations screen",
+            alt: "Groner - Solar Panels Installations Screen",
             src: "/companies-portfolio-photos/groner/6-groner-solar-panels-installations.png",
           },
           type: "photo",
-          title: "Solar panels installations screen",
+          title: "Solar Panels Installations Screen",
         },
         {
           photo: {
-            alt: "Groner - Email senders screen",
+            alt: "Groner - Email Senders Screen",
             src: "/companies-portfolio-photos/groner/7-groner-email-senders.png",
           },
           type: "photo",
-          title: "Email senders screen",
+          title: "Email Senders Screen",
         },
         {
           photo: {
-            alt: "Groner - Create task screen",
+            alt: "Groner - Create Task Screen",
             src: "/companies-portfolio-photos/groner/8-groner-create-task.png",
           },
           type: "photo",
-          title: "Create task screen",
+          title: "Create Task Screen",
         },
         {
           photo: {
-            alt: "Groner - Proposals list screen",
+            alt: "Groner - Proposals List Screen",
             src: "/companies-portfolio-photos/groner/9-groner-proposals-list.png",
           },
           type: "photo",
-          title: "Proposals list screen",
+          title: "Proposals List Screen",
         },
         {
           photo: {
-            alt: "Groner - Task tag list screen",
+            alt: "Groner - Task Tag List Screen",
             src: "/companies-portfolio-photos/groner/10-groner-task-tag-list.png",
           },
           type: "photo",
-          title: "Task tag list screen",
+          title: "Task Tag List Screen",
         },
         {
           photo: {
-            alt: "Groner - Task details screen",
+            alt: "Groner - Task Details Screen",
             src: "/companies-portfolio-photos/groner/11-groner-task-details.png",
           },
           type: "photo",
-          title: "Task details screen",
+          title: "Task Details Screen",
         },
         {
           photo: {
-            alt: "Groner - Deals list screen",
+            alt: "Groner - Deals List Screen",
             src: "/companies-portfolio-photos/groner/12-groner-deals-list.png",
           },
           type: "photo",
-          title: "Deals list screen",
+          title: "Deals List Screen",
         },
         {
           photo: {
-            alt: "Groner - Task list screen",
+            alt: "Groner - Task List Screen",
             src: "/companies-portfolio-photos/groner/13-groner-task-list.png",
           },
           type: "photo",
-          title: "Task list screen",
+          title: "Task List Screen",
         },
         {
           photo: {
-            alt: "Groner - User details routing tab",
+            alt: "Groner - User Details Routing Tab",
             src: "/companies-portfolio-photos/groner/14-groner-user-details-routing.png",
           },
           type: "photo",
-          title: "User details routing tab",
+          title: "User Details Routing Tab",
         },
         {
           photo: {
-            alt: "Groner - User details info tab",
+            alt: "Groner - User Details Info Tab",
             src: "/companies-portfolio-photos/groner/15-groner-user-details-info.png",
           },
           type: "photo",
-          title: "User details info tab",
+          title: "User Details Info Tab",
         },
         {
           photo: {
-            alt: "Groner - User details system tab",
+            alt: "Groner - User Details System Tab",
             src: "/companies-portfolio-photos/groner/17-groner-user-details-system.png",
           },
           type: "photo",
-          title: "User details system tab",
+          title: "User Details System Tab",
         },
         {
           photo: {
-            alt: "Groner - User details notifications tab",
+            alt: "Groner - User Details Notifications Tab",
             src: "/companies-portfolio-photos/groner/18-groner-user-details-notifications.png",
           },
           type: "photo",
-          title: "User details notifications tab",
+          title: "User Details Notifications Tab",
         },
         {
           photo: {
-            alt: "Groner - User details history tab",
+            alt: "Groner - User Details History Tab",
             src: "/companies-portfolio-photos/groner/19-groner-user-details-history.png",
           },
           type: "photo",
-          title: "User details history tab",
+          title: "User Details History Tab",
         },
         {
           photo: {
-            alt: "Groner - Lead details screen",
+            alt: "Groner - Lead Details Screen",
             src: "/companies-portfolio-photos/groner/20-groner-lead-details.png",
           },
           type: "photo",
-          title: "Lead details screen",
+          title: "Lead Details Screen",
         },
       ],
     },
